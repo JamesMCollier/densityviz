e8ce78910ae7f54623445383bb08bb074b652e09
James Collier <james.collier16987@outlook.com>
2021-07-28 12:28:59 -0500

    Resolve imports for function ridgelineBy(), update documentation for ridgelineBy()

--- R/ridgelineBy.R
+++ R/ridgelineBy.R
@@ -12,37 +12,38 @@
 #' @param ... arguments passed to scale_y_continuous() or scale_y_reverse()
 #'
 #' @return a gg or ggplot object
+#' @importFrom rlang .data
 #' @export
 #'
 #' @examples
-#' **do later
+#'
 ridgelineBy = function(starsObj, attr, dim, plotTitle, y_reverse = FALSE, sd = TRUE, ...){
-  if(sd == TRUE) range = c(-3 * sd(starsObj[[attr]], na.rm = TRUE), 3 * sd(starsObj[[attr]], na.rm = TRUE))
-  else range = c(min(as.numeric(starsObj[[attr]]), na.rm = TRUE), max(as.numeric(starsObj[[attr]]), na.rm = TRUE))
+  if(sd == TRUE) range = c(-3 * stats::sd(starsObj[[attr]], na.rm = TRUE), 3 * stats::sd(starsObj[[attr]], na.rm = TRUE))
+  else range = c(base::min(base::as.numeric(starsObj[[attr]]), na.rm = TRUE), base::max(base::as.numeric(starsObj[[attr]]), na.rm = TRUE))
 
-  df = data.frame(attr = as.vector(starsObj[[attr]]))
+  df = base::data.frame(attr = base::as.vector(starsObj[[attr]]))
   # num data points per time value
-  n = length(as.vector(starsObj[[attr]])) / length(st_get_dimension_values(starsObj, dim))
-  df$states = rep(st_get_dimension_values(starsObj, dim), each = n)
+  n = base::length(base::as.vector(starsObj[[attr]])) / base::length(stars::st_get_dimension_values(starsObj, dim))
+  df$states = base::rep(stars::st_get_dimension_values(starsObj, dim), each = n)
 
   # ggplot2 needs data in long form
-  gg = ggplot(df, aes(x = attr, y = states, fill = ..x..,
-                      group = states)) +
-    geom_density_ridges_gradient(scale = 3,rel_min_height = 0.01) +
-    scale_fill_distiller(palette = "RdBu", limits = range) +
-    theme_bw() + theme(
-      axis.title.x = element_text(size = 6),
-      axis.title.y = element_blank(),
-      axis.text.y = element_text(size = 5),
+  gg = ggplot2::ggplot(df, ggplot2::aes(x = .data$attr, y = .data$states, fill = .data$..x..,
+                      group = .data$states)) +
+    ggridges::geom_density_ridges_gradient(scale = 3,rel_min_height = 0.01) +
+    ggplot2::scale_fill_distiller(palette = "RdBu", limits = range) +
+    ggplot2::theme_bw() + ggplot2::theme(
+      axis.title.x = ggplot2::element_text(size = 6),
+      axis.title.y = ggplot2::element_blank(),
+      axis.text.y = ggplot2::element_text(size = 5),
       legend.position="none",
-      panel.spacing = unit(0.1, "lines"),
-      strip.text.x = element_text(size = 8)
-    ) + xlim(range) +
-    labs(title = plotTitle)
+      panel.spacing = ggplot2::unit(0.1, "lines"),
+      strip.text.x = ggplot2::element_text(size = 8)
+    ) + ggplot2::xlim(range) +
+    ggplot2::labs(title = plotTitle)
 
   if(y_reverse == TRUE){
-    return(gg + scale_y_reverse(...))
+    return(gg + ggplot2::scale_y_reverse(...))
   }else{
-    return(gg + scale_y_continuous(...))
+    return(gg + ggplot2::scale_y_continuous(...))
   }
 }
