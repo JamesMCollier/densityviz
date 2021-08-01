e67553dcedd022c68a1e6cde2f34e75b382452b4
James Collier <james.collier16987@outlook.com>
2021-08-01 14:52:08 -0500

    Resolve dependencies for animateHTMLBy()

--- R/animateHTMLBy.R
+++ R/animateHTMLBy.R
@@ -4,7 +4,7 @@
 #' @param attr attribute of interest
 #' @param dim grouping dimension
 #' @param title plot title
-#' @param anim_range if specified, range of dimension values along which to animate
+#' @param anim_range if specified, dimension values along which to animate
 #' @param ... arguments passed to plotDensityByDimensionValue()
 #'
 #' @return
@@ -13,8 +13,8 @@
 #' @examples
 #'
 animateHTMLBy = function(starsObj, attr, dim, title, anim_range = NULL, ...){
-  ani.options(interval = .4)
-  saveHTML(anim_factory(starsObj, attr, dim, title, ..., anim_range),
+  animation::ani.options(interval = .4)
+  animation::saveHTML(anim_factory(starsObj, attr, dim, title, ..., anim_range),
            autoplay = FALSE,
            loop = FALSE,
            verbose = FALSE,
@@ -28,11 +28,11 @@ animateHTMLBy = function(starsObj, attr, dim, title, anim_range = NULL, ...){
 anim_factory = function(starsObj, attr, dim, title, anim_range = NULL, ...){
   if(!is.null(anim_range)){
     for(i in anim_range){
-      print(plotDensityByDimensionValue(starsObj, attr, dim, i, title, ...))
+      base::print(plotDensityByDimensionValue(starsObj, attr, dim, i, title, ...))
     }
   }else{
-    for(i in unique(st_get_dimension_values(starsObj, dim))){
-      print(plotDensityByDimensionValue(starsObj, attr, dim, i, title, ...))
+    for(i in base::unique(stars::st_get_dimension_values(starsObj, dim))){
+      base::print(plotDensityByDimensionValue(starsObj, attr, dim, i, title, ...))
     }
   }
 }
@@ -53,66 +53,68 @@ anim_factory = function(starsObj, attr, dim, title, anim_range = NULL, ...){
 #' minimum and maximum of the attribute.
 #'
 #' @return
+#' @importFrom rlang .data
 #' @export
 #'
 #' @examples
 plotDensityByDimensionValue = function(starsObj, attr, dim, value, title, sf = NULL, baseline = NULL, ylim = c(0,0.5), xlim = NULL, sd = TRUE){
-  if(sd == TRUE) range = c(-3 * sd(starsObj[[attr]], na.rm = TRUE), 3 * sd(starsObj[[attr]], na.rm = TRUE))
-  else range = c(min(as.numeric(starsObj[[attr]]), na.rm = TRUE), max(as.numeric(starsObj[[attr]]), na.rm = TRUE))
+  if(sd == TRUE) range = base::c(-3 * stats::sd(starsObj[[attr]], na.rm = TRUE), 3 * stats::sd(starsObj[[attr]], na.rm = TRUE))
+  else range = base::c(base::min(base::as.numeric(starsObj[[attr]]), na.rm = TRUE), base::max(base::as.numeric(starsObj[[attr]]), na.rm = TRUE))
 
-  df = data.frame(attr = as.vector(starsObj[[attr]]))
+  df = base::data.frame(attr = base::as.vector(starsObj[[attr]]))
   # num data points per dim value
   # consider a different method, sometimes n is not an integer and an error occurs
   # depending on what starsObj is passed to the function
-  n = length(as.vector(starsObj[[attr]])) / length(st_get_dimension_values(starsObj, dim))
-  df$states = rep(st_get_dimension_values(starsObj, dim), each = n)
+  n = base::length(base::as.vector(starsObj[[attr]])) / base::length(stars::st_get_dimension_values(starsObj, dim))
+  df$states = base::rep(stars::st_get_dimension_values(starsObj, dim), each = n)
 
-  data_x = data_y = data_states = vector()
-  x = pull(filter(df, states == value, !is.na(attr)), attr)
-  y = density(x, n = 2^12, adjust = 3)
-  state = rep(i, 2^12)
+  data_x = data_y = data_states = base::vector()
+  x = dplyr::pull(dplyr::filter(df, .data$states == value, !base::is.na(attr)), attr)
+  y = stats::density(x, n = 2^12, adjust = 3)
+  state = base::rep(value, 2^12)
 
-  data_x = c(data_x, y$x)
-  data_y = c(data_y, y$y)
-  data_states = c(data_states, state)
+  data_x = base::c(data_x, y$x)
+  data_y = base::c(data_y, y$y)
+  data_states = base::c(data_states, state)
 
-  data = data.frame(x = data_x, y = data_y, states = data_states)
+  data = base::data.frame(x = data_x, y = data_y, states = data_states)
 
   # ggplot2 needs data in long form, and geom_density() doesn't have the
   # functionality for color fill we'd like, so there are a few extra steps above
-  gg = ggplot(data, aes(x, y)) +
-    geom_segment(aes(xend = x, yend = 0, color = x)) +
-    scale_color_distiller(palette = "RdBu", limits = range) +
-    geom_line() +
-    xlim(range) +
-    ylim(ylim) +
-    labs(title = paste(title, value))
+  gg = ggplot2::ggplot(data, ggplot2::aes(.data$x, .data$y)) +
+    ggplot2::geom_segment(ggplot2::aes(xend = .data$x, yend = 0, color = .data$x)) +
+    ggplot2::scale_color_distiller(palette = "RdBu", limits = range) +
+    ggplot2::geom_line() +
+    ggplot2::xlim(range) +
+    ggplot2::ylim(ylim) +
+    ggplot2::labs(title = base::paste(title, value))
 
-  if(!is.null(baseline)){
-    gg = gg + geom_density(data = filter(df, between(states, baseline[1], baseline[2]), !is.na(attr)),
-                           adjust = 3, inherit.aes = FALSE, mapping = aes(x=attr),
+  if(!base::is.null(baseline)){
+    d1 = dplyr::filter(df, dplyr::between(.data$states, baseline[1], baseline[2]), !base::is.na(attr))
+    gg = gg + ggplot2::geom_density(data = d1,
+                           adjust = 3, inherit.aes = FALSE, mapping = ggplot2::aes(x=.data$attr),
                            linetype = "dotted")
   }
 
-  if(!is.null(sf)){
-    sf$avg = rep(mean(pull(filter(df, states == value), attr), na.rm = TRUE), nrow(sf))
-    print(unique(sf$avg))
-    m1 = ggplot(st_as_sf(sf)) + geom_sf(aes(fill = avg)) +
-      coord_sf(expand = FALSE) +
-      scale_fill_distiller(palette = "RdBu", limits = range) +
-      theme(
-        axis.title = element_blank(),
-        plot.title = element_text(size = 8),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
+  if(!base::is.null(sf)){
+    sf$avg = base::rep(base::mean(dplyr::pull(dplyr::filter(df, .data$states == value), attr), na.rm = TRUE), base::nrow(sf))
+    base::print(base::unique(sf$avg))
+    m1 = ggplot2::ggplot(sf::st_as_sf(sf)) + ggplot2::geom_sf(ggplot2::aes(fill = .data$avg)) +
+      ggplot2::coord_sf(expand = FALSE) +
+      ggplot2::scale_fill_distiller(palette = "RdBu", limits = range) +
+      ggplot2::theme(
+        axis.title = ggplot2::element_blank(),
+        plot.title = ggplot2::element_text(size = 8),
+        axis.text = ggplot2::element_blank(),
+        axis.ticks = ggplot2::element_blank(),
         legend.position = "none"
       )
 
     x.min = .3 * range[2]
     x.max = .9 * range[2]
 
-    gg = gg + annotation_custom(
-      grob = ggplotGrob(m1),
+    gg = gg + ggplot2::annotation_custom(
+      grob = ggplot2::ggplotGrob(m1),
       xmin = x.min,
       xmax = x.max,
       ymin = .35,
@@ -120,8 +122,8 @@ plotDensityByDimensionValue = function(starsObj, attr, dim, value, title, sf = N
     )
   }
 
-  if(!is.null(xlim)){
-    gg = gg + xlim(xlim)
+  if(!base::is.null(xlim)){
+    gg = gg + ggplot2::xlim(xlim)
     range = xlim
   }
 
