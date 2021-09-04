f778c35884632d4b214a126ba8ddbfc104e2107f
James Collier <james.collier16987@outlook.com>
2021-09-04 16:13:42 -0500

    add anomalize function, resolve dependencies, create documentation, create testthat file (unit test still to be done)

---
+++ R/anomalize.R
@@ -0,0 +1,56 @@
+#' Anomalize Datasets
+#'
+#' Redefine data points as how far away they are from the average for that point in space, a useful tool for better visualization and analysis
+#'
+#' @param starsObj a stars object
+#' @param attr attribute of stars object
+#' @param space1 spatial dimension (e.g., station, Longitude)
+#' @param space2 second spatial dimension, if applicable (e.g., Latitude)
+#' @param time time dimension
+#' @param baseline optional parameter that chooses which time period to calculate anomalies for (e.g., 1951-1980)
+#' @param ... other dimensions not involved in anomalization, but still part of the original stars object
+#'
+#' @return a stars object with anomalized attribute
+#' @export
+#'
+#' @examples
+#'
+anomalize = function(starsObj, attr, space1, space2 = NULL, time, baseline = NULL, ...){
+  x = stars::st_get_dimension_values(starsObj, space1)
+
+  # need to redefine baseline a bit for subsetting
+  if(base::is.null(baseline)) baseline = 1:base::length(st_get_dimension_values(starsObj,time))
+  else{
+    z = stars::st_get_dimension_values(starsObj, time)
+    baseline = base::which(z %in% baseline)
+  }
+
+  i = j = 1
+  # Say Long and Lat are provided
+  if(!base::is.null(space2)){
+    y = stars::st_get_dimension_values(starsObj, space2)
+    anoms = base::array(dim = c(
+      base::length(x),
+      base::length(y)
+    ))
+    for(i1 in x){
+      j = 1
+      for(j1 in y){
+        starsObj = base::aperm(starsObj, c(space1,space2,time,...))
+        array = starsObj[attr, i, j, baseline][[1]]
+
+        anoms[i,j] = base::mean(array, na.rm = TRUE)
+
+        j = j + 1
+      }
+      i = i + 1
+    }
+  }
+
+  units::units(anoms) = units::units(starsObj[[attr]]) = NULL
+
+  # we need to rely a bit on R's Vector Recycling Rules
+  # is this the best way?
+  starsObj[[attr]] = starsObj[[attr]] - base::as.vector(anoms)
+  return(starsObj)
+}
